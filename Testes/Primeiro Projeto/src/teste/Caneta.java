b530c26c7aede1cd08024c50af6cc4734fc26ba0
Matheus Costa <matheuscostamc135@gmail.com>
2023-08-04 18:28:15 -0300

    Getter, Setter e Constructor

--- Testes/Primeiro Projeto/src/teste/Caneta.java
+++ Testes/Primeiro Projeto/src/teste/Caneta.java
@@ -6,11 +6,44 @@ public class Caneta {
     protected int carga;
     private boolean tampada;
 
+    public Caneta(String modelo, String cor, float ponta, int carga) {
+        this.modelo = modelo;
+        this.cor = cor;
+        this.ponta = ponta;
+        this.carga = carga;
+        this.tampar();
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+    public String getCor() {
+        return cor;
+    }
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+    public float getPonta() {
+        return ponta;
+    }
+    public void setPonta(float ponta) {
+        this.ponta = ponta;
+    }
+    public int getCarga() {
+        return carga;
+    }
+    public void setCarga(int carga) {
+        this.carga = carga;
+    }
+
     public void status() {
-        System.out.println("Modelo:  " + this.modelo);
-        System.out.println("Cor: " + this.cor);
-        System.out.println("Ponta: " + this.ponta + "mm");
-        System.out.println("Carga: " + this.carga + "%");
+        System.out.println("Modelo:  " + this.getCarga());
+        System.out.println("Cor: " + this.getCor());
+        System.out.println("Ponta: " + this.getPonta() + "mm");
+        System.out.println("Carga: " + this.getCarga() + "%");
         System.out.println("Tampada: " + this.tampada);
     }
     public void rabiscar() {
--- Testes/Primeiro Projeto/src/teste/Teste.java
+++ Testes/Primeiro Projeto/src/teste/Teste.java
@@ -1,11 +1,7 @@
 package teste;
 public class Teste {
     public static void main(String[] args) throws Exception {
-        Caneta caneta1 = new Caneta();
-        caneta1.modelo = "Cis Spiro";
-        caneta1.cor = "Azul e Branco";
-        caneta1.ponta = 0.7f;
-        caneta1.carga = 70;
+        Caneta caneta1 = new Caneta("Cis Spiro", "Azul e Branco", 0.7f, 100);
         caneta1.destampar();
         caneta1.rabiscar();
         caneta1.tampar();
